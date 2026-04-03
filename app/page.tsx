1b5b1457ec0a74e10c608e4318ae448d0c2a2154
hanumagouda <hanumagoudabanakar5@gmail.com>
2026-04-03 07:16:31 +0530

    Update app/page.tsx to import Header component and create dashboard layout.

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+
+const DashboardLayout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Main content goes here */}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
